1427fcf00ac46f760b4115c42c170773efdf407f
xalton <xalton94@gmail.com>
2019-11-08 13:20:17 +0100

    delete previous functions.py

--- Rick_And_Morty_Dataset/Fucntions.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-import timeit
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
