f07122601eded9be6eaf5a36caf48fe52a18d1de
xalton <xalton94@gmail.com>
2019-11-08 22:43:46 +0100

    setting geometry and title on the main page

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ from tensorflow.keras.callbacks import TensorBoard
 from functions import DataPreparation,PlotImages,CreateModel
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+###SetUp Main Window###
 mainapp = tkinter.Tk()
-mainapp.title("Rick or Morty ?")
+title = "Rick or Morty ?"
+mainapp.title(title)
 
 screen_x = int(mainapp.winfo_screenwidth())
 screen_y = int(mainapp.winfo_screenheight())
@@ -22,6 +24,30 @@ window_y = 800
 posX = (screen_x // 2)-(window_x // 2)
 posY = (screen_y // 2)-(window_y // 2)
 
-geo = "{}*{}+{}+{}".format(window_x,window_y,posX,posY)
+geo = "{}x{}+{}+{}".format(window_x,window_y,posX,posY)
+mainapp.minsize(800,600)
+mainapp.maxsize(1920,1080)
+mainapp.geometry(geo)
 
-mainapp.mainloop(geo)
+label_title = tkinter.Label(mainapp, text=title)
+#font30 = "-family {DejaVu Sans} size 30 -weight normal"
+label_title.configure(font= '30')
+
+
+
+
+label_title.pack()
+mainapp.mainloop()
+
+
+
+#Input name
+#entry_name = tkinter.Entry(mainapp,width=25)
+#entry_name.pack()
+
+#Button
+#button_Test = tkinter.Button(mainapp, text='Test',command = name_func)
+
+#Check
+#check_widget = tkinter.Checkbutton(mainapp,text='')
+#check_widget.pack()
